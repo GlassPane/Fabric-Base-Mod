107645adbba4da3c3edb7defa25649956831b9ed
UpcraftLP <j-schirra@gmx.de>
2019-02-21 11:32:33 +0100

    initial files

---
+++ src/main/java/com/github/glasspane/basemod/BaseMod.java
@@ -0,0 +1,14 @@
+package com.github.glasspane.basemod;
+
+import com.github.glasspane.mesh.api.annotation.CalledByReflection;
+import net.fabricmc.api.ModInitializer;
+
+@CalledByReflection
+public class BaseMod implements ModInitializer {
+
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/glasspane/basemod/mixin/client/package-info.java
@@ -0,0 +1,1 @@
+package com.github.glasspane.basemod.mixin.client;
